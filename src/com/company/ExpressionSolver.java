d0000d00b163d1a3975f3c1c49602f1726c260f4
davidkenesaw <davidkennesaw@gmail.com>
2022-03-23 16:47:03 -0400

    fixed

---
+++ src/com/company/ExpressionSolver.java
@@ -0,0 +1,139 @@
+package com.company;
+
+import java.util.Stack;
+
+public class ExpressionSolver {
+    static Boolean isOperand(char c)
+    {
+        // If the character is a digit
+        // then it must be an operand
+        if (c >= 48 && c <= 57)
+            return true;
+        else
+            return false;
+    }
+    static int evaluatePostfix(String exp)
+    {
+        //create a stack
+        Stack<Integer> stack=new Stack<>();
+
+        // Scan all characters one by one
+        for(int i=0;i<exp.length();i++)
+        {
+            char c=exp.charAt(i);
+
+            // If the scanned character is an operand (number here),
+            // push it to the stack.
+            if(Character.isDigit(c))
+                stack.push(c - '0');
+
+                //  If the scanned character is an operator, pop two
+                // elements from stack apply the operator
+            else
+            {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch(c)
+                {
+                    case '+':
+                        stack.push(val2+val1);
+                        break;
+
+                    case '-':
+                        stack.push(val2- val1);
+                        break;
+
+                    case '/':
+                        stack.push(val2/val1);
+                        break;
+
+                    case '*':
+                        stack.push(val2*val1);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    static int Prec(char ch)
+    {
+        switch (ch)
+        {
+            case '+':
+            case '-':
+                return 1;
+
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    // The main method that converts
+    // given infix expression
+    // to postfix expression.
+    static String infixToPostfix(String exp)
+    {
+        // initializing empty String for result
+        String result = new String("");
+
+        // initializing empty stack
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i<exp.length(); ++i)
+        {
+            char c = exp.charAt(i);
+
+            // If the scanned character is an
+            // operand, add it to output.
+            if (Character.isLetterOrDigit(c))
+                result += c;
+
+                // If the scanned character is an '(',
+                // push it to the stack.
+            else if (c == '(')
+                stack.push(c);
+
+                //  If the scanned character is an ')',
+                // pop and output from the stack
+                // until an '(' is encountered.
+            else if (c == ')')
+            {
+                while (!stack.isEmpty() &&
+                        stack.peek() != '(')
+                    result += stack.pop();
+
+                stack.pop();
+            }
+            else // an operator is encountered
+            {
+                while (!stack.isEmpty() && Prec(c)
+                        <= Prec(stack.peek())){
+
+                    result += stack.pop();
+                }
+                stack.push(c);
+            }
+
+        }
+
+        // pop all the operators from the stack
+        while (!stack.isEmpty()){
+            if(stack.peek() == '(')
+                return "Invalid Expression";
+            result += stack.pop();
+        }
+        return result;
+    }
+
+    public static int solve(String value){
+        return evaluatePostfix(infixToPostfix(value));
+    }
+
+}
