9a280166f6b47cdf87bd85e48019db81b9030b2e
davidkenesaw <davidkennesaw@gmail.com>
2022-04-01 22:27:30 -0400

    fixed

--- src/com/company/ExpressionSolver.java
+++ src/com/company/ExpressionSolver.java
@@ -4,11 +4,9 @@ import org.python.util.PythonInterpreter;
 
 public class ExpressionSolver {
 
-
     public static String solve(String value){
         PythonInterpreter py = new PythonInterpreter();
         return py.eval(value).toString();
     }
-
 
 }
