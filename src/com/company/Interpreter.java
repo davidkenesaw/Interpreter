8ebb283f7b9ca6c5cd018865111302885b7dedee
davidkenesaw <davidkennesaw@gmail.com>
2022-04-12 22:04:46 -0400

    fixed

--- src/com/company/Interpreter.java
+++ src/com/company/Interpreter.java
@@ -17,42 +17,53 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Interpreter {
-
+    //storage for body of a while loop
     ArrayList<Scan> WhileBody = new ArrayList<Scan>();
-
+    //queue that contains all lexemes and tokens
     Queue<Scan> tokenInterpret = new LinkedList<Scan>();
+    //storage for all variables that are assigned a value
     ArrayList<VariableAssignment> variables = new ArrayList<VariableAssignment>();
 
     //constructor
     public Interpreter(ArrayList<Scan> array) {
-        AddtoQueue(array);
+        AddtoQueue(array,tokenInterpret);
         tokenInterpret.remove();
         tokenInterpret.remove();
     }
     //==============================Statement==================================
 
+    //Driver function that checks what the current statement is
     public void interpret(Queue<Scan> original){
+        //loop until queue is empty
         while (!original.isEmpty()) {
             if (original.peek().getToken().equals("id")) {
+                //if Statement is an assignment statement
                 AssignmentStatement(original);
             }else if(original.peek().getToken().equals("print_keyword")){
+                //if statement is a print statement
                 PrintStatement(original);
             }
             else if(original.peek().getToken().equals("while_keyword")){
+                //if statement is a While statement
                 WhileStatement(original);
             }else if(original.peek().getToken().equals("if_keyword")){
+                //if statement is an if else statement
                 IfStatement(original);
             }
             else if(original.peek().getToken().equals("end_keyword")){
+                //if end Keyword, remove it
                 original.remove();
             }
         }
     }
+    //function used to handle if statements
     public void IfStatement(Queue<Scan> original){
+        //store if body
         Queue<Scan> IfBody = new LinkedList<Scan>();
+        //store else body
         Queue<Scan> ElseBody = new LinkedList<Scan>();
 
-
+        //store values needed for boolean expression
         original.remove();
         String repOperator = original.peek().getLexeme();
         original.remove();
@@ -62,8 +73,8 @@ public class Interpreter {
         original.remove();
 
         original.remove();
-        //System.out.println(repOperator+" "+variable+" "+Limit);
 
+        //store the if and else body statements
         while(!original.peek().getToken().equals("else_keyword")){
             IfBody.add(original.peek());
             original.remove();
@@ -75,21 +86,20 @@ public class Interpreter {
             original.remove();
         }
         ElseBody.add(new Scan("end",tokenInterpret.peek().getLineNumber()));
-        //printQueue(IfBody);
-        //System.out.println();
-        //printQueue(ElseBody);
+
 
         if(booleanExpression(getValueOfVariable(variable),Limit,repOperator)){
+            //if boolean expression returns true
             interpret(IfBody);
         }else{
+            //if boolean expression returns false
             interpret(ElseBody);
         }
-
-
     }
 
+    //first function used to deal with while statements
     public void WhileStatement(Queue<Scan> original){
-
+        //store values needed for boolean expression
         original.remove();
         String repOperator = original.peek().getLexeme();
         original.remove();
@@ -100,54 +110,56 @@ public class Interpreter {
 
         original.remove();
 
+        //call second function needed for while statements
         WhileBlock(variable,Limit,repOperator);
 
     }
+    //second function used for dealing with while statements
     public void WhileBlock(String left, String right, String rep_op){
+        //temporarily store body of while loop
         Queue<Scan> temp = new LinkedList<Scan>();
 
+        //temporarily store body of while loop
         while(!tokenInterpret.peek().getToken().equals("end_keyword")){
             WhileBody.add(tokenInterpret.peek());
             tokenInterpret.remove();
         }
         WhileBody.add(tokenInterpret.peek());
-        //printArrayList(WhileBody);
 
-        AddtoSpecificQueue(WhileBody,temp);
-        //printQueue(temp);
+        AddtoQueue(WhileBody,temp);
 
-
-        //interpret(temp);
-
+        //execute while loop
         while(booleanExpression(getValueOfVariable(left),right,rep_op)){
 
             interpret(temp);
-            AddtoSpecificQueue(WhileBody,temp);
+            AddtoQueue(WhileBody,temp);
 
         }
-
+        //clear while loop array list
         WhileBody.clear();
     }
 
+    //function for dealing with assignment statements
     public void AssignmentStatement(Queue<Scan> original){
-
+        //variable name
         String variable = original.peek().getLexeme();
         original.remove();
         original.remove();
 
 
-
+        //everything after equal sign
         String expression = ArithmeticExpression(original);
-        //System.out.println(variable+"="+expression + " = " + ExpressionSolver.solve(expression));
 
         if(variableContains(variable)){
+            //if variable already exists
             variables.set(variableGetIndex(variable),new VariableAssignment(variable,ExpressionSolver.solve(expression)));
         }else{
-
+            //if variable hasn't been initialized yet
             variables.add(new VariableAssignment(variable,ExpressionSolver.solve(expression)));
         }
     }
 
+    //function for dealing with print statements
     public void PrintStatement(Queue<Scan> original){
         original.remove();
         String expression = ArithmeticExpression(original);
@@ -156,14 +168,18 @@ public class Interpreter {
 
     //==============================Expression==================================
 
+    //function for dealing with Arithmetic expressions
     public String ArithmeticExpression(Queue<Scan> original){
+        //stores expression in string
         String expression = "";
         int CurrentLine = original.peek().getLineNumber();
         while (CurrentLine==original.peek().getLineNumber()){
             if(original.peek().getToken().equals("id")){
+                //if lexeme is a variable
                 expression += getValueOfVariable(original.peek().getLexeme());
             }
             else{
+                //if lexeme is an integer
                 expression += original.peek().getLexeme();
             }
             original.remove();
@@ -171,24 +187,32 @@ public class Interpreter {
         return expression;
     }
     public boolean booleanExpression(String left, String right, String repOp){
+        //left side of equal statement
         int IntLeft = Integer.parseInt(left);
+        //right side of equal statement
         int IntRight = Integer.parseInt(right);
         if(repOp.equals("<")){
+            //less than
             if(IntLeft < IntRight)return true;
             else return false;
         }else if(repOp.equals("<=")){
+            //less than or equal to
             if(IntLeft <= IntRight)return true;
             else return false;
         }else if(repOp.equals(">")){
+            //greater than
             if(IntLeft > IntRight)return true;
             else return false;
         }else if(repOp.equals(">=")){
+            //greater than or equal to
             if(IntLeft >= IntRight)return true;
             else return false;
         }else if(repOp.equals("==")){
+            //equal to
             if(IntLeft == IntRight)return true;
             else return false;
         }else if(repOp.equals("~=")){
+            //not equal to
             if(IntLeft != IntRight)return true;
             else return false;
         }
@@ -196,19 +220,15 @@ public class Interpreter {
     }
     //==============================Helper==================================
 
-    public void AddtoQueue(ArrayList<Scan> array){
-        //fills queue with tokens
-        for(int loop = 0; loop < array.size(); loop++){
-            tokenInterpret.add(array.get(loop));
-        }
-    }
-    public void AddtoSpecificQueue(ArrayList<Scan> array, Queue<Scan> temp){
-        //fills queue with tokens
+
+    public void AddtoQueue(ArrayList<Scan> array, Queue<Scan> temp){
+        //fills a specific queue with tokens
         for(int loop = 0; loop < array.size(); loop++){
             temp.add(array.get(loop));
         }
     }
     public boolean variableContains(String Value){
+        //checks if variable array list contains the string variable
         for(int loop = 0; loop < variables.size(); loop++){
             if(variables.get(loop).getVariable().equals(Value)){
                 return true;
@@ -217,6 +237,7 @@ public class Interpreter {
         return false;
     }
     public int variableGetIndex(String Value){
+        //get index of variable of in array list
         for(int loop = 0; loop < variables.size(); loop++){
             if(variables.get(loop).getVariable().equals(Value)){
                 return loop;
@@ -225,6 +246,7 @@ public class Interpreter {
         return -1;
     }
     public String getValueOfVariable(String variable){
+        //get the value of the variable
         for(int loop = 0; loop < variables.size(); loop++){
             if(variables.get(loop).getVariable().equals(variable)){
                 return variables.get(loop).getValue();
@@ -232,24 +254,15 @@ public class Interpreter {
         }
         return "couldnt find it";
     }
-    public void print() {
-        while(!tokenInterpret.isEmpty()){
-            System.out.println(tokenInterpret.peek().toString());
-            tokenInterpret.remove();
-        }
-    }
-    public void printVariables(){
-        for(int loop = 0; loop < variables.size(); loop++){
-            System.out.println(variables.get(loop).toString());
-        }
-    }
     public void printQueue(Queue<Scan> temp){
+        //prints a queue
         while(!temp.isEmpty()){
             System.out.println(temp.peek().toString());
             temp.remove();
         }
     }
     public void printArrayList(ArrayList<Scan> temp){
+        //print array list
         for(int loop = 0; loop < temp.size(); loop++){
             System.out.println(temp.get(loop).toString());
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,75 +17,68 @@
  * Project:     Deliverable P2 Parser
  */
 
+//and
+
+/*
+ * Class:       CS 4308 Section 3
+ * Term:        Spring
+ * Name:       David VanAsselberg
+ * Instructor:   Sharon Perry
+ * Project:     Deliverable P3 Interpreter
+ */
+
 package com.company;
 
 import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-
-    public static ArrayList<Scan> SymbolTable = new ArrayList<Scan>();
     //array list used for storing tokens and lexemes
-    public static BufferedReader reader=null;
+    public static ArrayList<Scan> SymbolTable = new ArrayList<Scan>();
     //reads to file
-
-    //writes to file
-    public static ArrayList<Integer> LineNumbers = new ArrayList<Integer>();
+    public static BufferedReader reader=null;
     //stores the line number
+    public static ArrayList<Integer> LineNumbers = new ArrayList<Integer>();
+
     public static void main(String[] args) throws IOException {
-
-        String InputFile = "inputfile.txt";
         //source code file
-        String OutputFile = "output.txt";
-        //Symbol Table file
-
-
+        String InputFile = "inputfile.txt";
 
         //Scanner
         try{
-            scan2(InputFile);
             //method used to scan input file to build the symbol table
-
+            scan2(InputFile);
         }catch (IOException e){
-            e.printStackTrace();
             //display exception
+            e.printStackTrace();
         }
         finally {
+            //close the reader
             reader.close();
-
-            //close the reader and writer
         }
-        System.out.println("==================================Scanner=====================================");
-        System.out.println();
-        printTable();
-        System.out.println();
-        System.out.println("==================================Parser======================================");
 
-        //Parser
+        //parse and interpret the symbol table
         Parser parse = new Parser(SymbolTable);
-        System.out.println();
-        parse.function();
-        parse.PARSETREE();
-        System.out.println();
-        System.out.println();
-
-        System.out.println("==================================Interpreter==================================");
-        System.out.println();
-        //Interpreter
         Interpreter interpret = new Interpreter(SymbolTable);
-        interpret.interpret(interpret.tokenInterpret);
 
+        System.out.println("==================================Scanner=====================================\n");
+        printTable();
+        System.out.println("\n==================================Parser======================================\n");
+        if(parse.function() == true){
+            parse.PARSETREE();
+            System.out.println("\n\n==================================Interpreter==================================");
+            interpret.interpret(interpret.tokenInterpret);
+        }
 
 
     }
     public static void scan2(String input) throws IOException {
+        //scans lexemes into symbol table
         reader = new BufferedReader(new FileReader(input));
-
 
         int LineNumber = 1;
         String line;
         while((line = reader.readLine()) != null){
-            //System.out.println(line);
             if(!line.isBlank()) {
                 line = line.trim();
                 String[] CurrentLine = line.split("[()\\s]+");
@@ -97,11 +90,13 @@ public class Main {
         }
     }
     public static void printTable(){
+        //print symbol table
         for(int loop = 0; loop < SymbolTable.size(); loop++){
             System.out.println(SymbolTable.get(loop).toString());
         }
     }
     public static void AddToTable(String[] array, int lineNumber){
+        //add to symbol table
         for(int loop = 0; loop < array.length; loop++){
             SymbolTable.add(new Scan(array[loop],lineNumber));
         }
