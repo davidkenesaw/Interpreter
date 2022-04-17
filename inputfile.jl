b334554fab7721149637e4f547377f212e0a05e0
davidkenesaw <davidkennesaw@gmail.com>
2022-04-17 02:12:58 -0400

    fixed

--- inputfile.jl
+++ inputfile.jl
@@ -1,14 +1,14 @@
+
 function a()
 
-
-
+    x = 7
+    y = x + 3
     x = 4
+    z = 4 + x / 2 * 3
 
-    if < x 4 then
-        print(1)
-    else
-        print(2)
-    end
+    print(x)
+    print(y)
+    print(z)
 
 
 end
--- src/com/company/ExpressionSolver.java
+++ src/com/company/ExpressionSolver.java
@@ -13,6 +13,7 @@ import org.python.util.PythonInterpreter;
 
 public class ExpressionSolver {
 
+    //solves mathematical expressions
     public static String solve(String value){
         PythonInterpreter py = new PythonInterpreter();
         return py.eval(value).toString();
